7a8f9289e531698e92eb5f15c535993ba9794e35
lspc678 <dltmdqls1945@naver.com>
2023-12-13 23:55:18 +0900

    chore: Enable message queue use

--- app/os_cfg.h
+++ app/os_cfg.h
@@ -91,11 +91,11 @@
 
 
                                        /* ---------------------- MESSAGE QUEUES ---------------------- */
-#define OS_Q_EN                   0    /* Enable (1) or Disable (0) code generation for QUEUES         */
-#define OS_Q_ACCEPT_EN            0    /*     Include code for OSQAccept()                             */
+#define OS_Q_EN                   1    /* Enable (1) or Disable (0) code generation for QUEUES         */
+#define OS_Q_ACCEPT_EN            1    /*     Include code for OSQAccept()                             */
 #define OS_Q_DEL_EN               0    /*     Include code for OSQDel()                                */
 #define OS_Q_FLUSH_EN             0    /*     Include code for OSQFlush()                              */
-#define OS_Q_POST_EN              0    /*     Include code for OSQPost()                               */
+#define OS_Q_POST_EN              1    /*     Include code for OSQPost()                               */
 #define OS_Q_POST_FRONT_EN        0    /*     Include code for OSQPostFront()                          */
 #define OS_Q_POST_OPT_EN          0    /*     Include code for OSQPostOpt()                            */
 #define OS_Q_QUERY_EN             0    /*     Include code for OSQQuery()                              */
